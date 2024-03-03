20dcf00349c223228ecf7eb12b874a03b6231912
Mark Blumenthal <markblumenthal@live.com>
2024-03-03 14:35:56 +0200

    changes

---
+++ week2/day1/exercises-xp/exercises-xp.py
@@ -0,0 +1,61 @@
+#exercise 1
+#print("Hello world\n" * 4)
+# answer correct
+
+#exercise 2
+#result = (99**3) * 8
+#print(result)
+# answer-7762392
+
+#exercise 3
+#print(5 < 3) =false
+#print(3==3) =true
+#print(3=="3") =false
+#print("3" > 3) =typeError
+#print("Hello" == "hello") =false
+
+#exercise 4
+#computer_brand = "Dell"  
+#print("I have a", computer_brand, "computer")
+#answer correct
+
+#exercise 5
+#name = "Mark"
+#age = 35
+#shoe_size = 7
+#info = f"My name is {name}, and I am {age} years old, and my shoe size is {shoe_size} which is very small."
+#print(info)
+#answer correct
+
+#exercise 6
+#a = 10  
+#b = 5   
+#if a > b:
+ #print("Hello World")
+#answer correct
+
+#exercise 7
+#number = int(input("Enter a number: "))
+#if number % 2 == 0:
+    #print(f"{number} is even.")
+#else:
+    #print(f"{number} is odd.")
+    #answer correcr
+
+#exercise 8
+#my_name = "Mark"
+#user_name = input("Enter your name: ")
+#if user_name == my_name:
+    #print("OMG!!!, we have the same name! but who is better?")
+#else:
+    #print(f"Nice to meet you, {user_name}! I do not know you, but lets be friends")
+#answer correct
+
+#exercise 9
+#height_inches = float(input("Enter your height in inches: "))
+#height_cm = height_inches * 2.54
+#if height_cm > 145:
+    #print("You are tall enough to ride!")
+#else:
+    #print("Sorry, you need to grow some more to ride.")
+#answer correct
