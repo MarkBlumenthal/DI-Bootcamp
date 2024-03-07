4981b65a1e8580213c6436e7267b4d623f89a043
Mark Blumenthal <markblumenthal@live.com>
2024-03-07 09:27:19 +0200

    changes

---
+++ week2/day5/daily-challenge.py
@@ -0,0 +1,33 @@
+# #Daily Challenge -1
+
+# def sort_words(input_string):
+
+#     words = input_string.split(',')
+#     sorted_words = sorted([word for word in words])
+#     return ','.join(sorted_words)
+
+
+# input_string = input("Enter a comma-separated sequence of words: ")
+
+# print(sort_words(input_string))
+
+# input_string = input("Enter a comma-separated sequence of words: ")
+
+# print(sort_words(input_string))
+# def sort_words(input_string):
+   
+#     words = input_string.split(',')
+  
+#     sorted_words = sorted([word for word in words])
+  
+#     return ','.join(sorted_words)
+
+# input_string = input("Enter a comma-separated sequence of words: ")
+
+# print(sort_words(input_string))
+#answer - correct
+
+
+
+
+#Challenge - 2
