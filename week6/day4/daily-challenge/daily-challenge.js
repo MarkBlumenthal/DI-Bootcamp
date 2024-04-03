39c6ca29feae113b73af1f10e30c7ac80caecb52
Mark Blumenthal <markblumenthal@live.com>
2024-04-03 15:28:52 +0300

    changes

--- week6/day4/daily-challenge/daily-challenge.js
+++ week6/day4/daily-challenge/daily-challenge.js
@@ -1,11 +1,21 @@
+
+
 function submitForm() {
-  
+
     const name = document.querySelector('#name').value;
     const lastName = document.querySelector('#lastName').value;
-
     
     const jsonOutput = { name, lastname: lastName };
-
-
-    document.querySelector('#output').textContent = JSON.stringify(jsonOutput, null, 2);
+    
+    const pre = document.createElement('pre');
+    
+    pre.textContent = JSON.stringify(jsonOutput, null, 2);
+    
+    let outputElement = document.querySelector('#output');
+    if (outputElement) {
+        outputElement.textContent = pre.textContent;
+    } else {
+        pre.id = 'output';
+        document.body.appendChild(pre);
+    }
 }
