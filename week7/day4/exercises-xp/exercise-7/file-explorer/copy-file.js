d69b6a62566e2d93884fcf5e9fac48ccf29e53d8
Mark Blumenthal <markblumenthal@live.com>
2024-04-10 16:35:21 +0300

    changes

---
+++ week7/day4/exercises-xp/exercise-7/file-explorer/copy-file.js
@@ -0,0 +1,13 @@
+// const fs = require('fs').promises;
+
+// async function copyFile() {
+//   try {
+//     const data = await fs.readFile('source.txt', 'utf8');
+//     await fs.writeFile('destination.txt', data);
+//     console.log('File copied successfully');
+//   } catch (error) {
+//     console.error('Error copying file:', error);
+//   }
+// }
+
+// copyFile();
---
+++ week7/day4/exercises-xp/exercise-7/file-explorer/read-directory.js
@@ -0,0 +1,12 @@
+// const fs = require('fs').promises;
+
+// async function readDirectory() {
+//   try {
+//     const files = await fs.readdir('.');
+//     console.log('Files in the directory:', files);
+//   } catch (error) {
+//     console.error('Error reading directory:', error);
+//   }
+// }
+
+// readDirectory();
